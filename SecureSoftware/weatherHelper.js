0d80ea62ba475e67af6cdb35e9f5356a447e7e80
Ian Nevius <ipn1@pct.edu>
2023-03-28 19:06:09 -0400

    Validating Input from API by location name

--- SecureSoftware/weatherHelper.js
+++ SecureSoftware/weatherHelper.js
@@ -1,5 +1,4 @@
 
-
 var temp = 100;
 
 var weatherBox = document.getElementById("weatherDisplay");
@@ -46,15 +45,12 @@ function submit() {
     } else {
 
     let request = new XMLHttpRequest();
-    //const url = `http://api.openweathermap.org/data/2.5/weather?q=${zip}&appid=[YOUR-API-KEY-HERE]`;
     const cururl = `http://api.weatherapi.com/v1/forecast.json?key=62433828d6e84f108e1185936232201&q=${zip}&days=3&aqi=no&alerts=no`;
-    //const foreurl = `http://api.weatherapi.com/v1/forecast.json?key=&q=${zip}&days=3&aqi=no&alerts=no`;
 
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
-        const foreresponse = JSON.parse(this.responseText);
         getElements(response);
         //getForeElements(foreresponse);
       }
@@ -62,29 +58,36 @@ function submit() {
 
 
     function getElements(response) {
-        //document.getElementById("temp").innerHTML = (`The humidity in ${zip} is ${response.current.temp_f}`);
-        temp = response.current.temp_f;
-        weatherBox.style.visibility = "visible";
-  
-        changeColor(temp, "weatherDisplay");
-        document.getElementById("location").innerText = `${response.location.name}`;
-        document.getElementById("temp").innerText = "Temp: " + temp + "°"
-        document.getElementById("condition").innerText = "Condition: " + response.current.condition.text;
+        //Validating input from the api
+        if(response.location.name.length > 0) {
+
+            temp = response.current.temp_f;
+            weatherBox.style.visibility = "visible";
+    
+            changeColor(temp, "weatherDisplay");
+            document.getElementById("location").innerText = `${response.location.name}`;
+            document.getElementById("temp").innerText = "Temp: " + temp + "°"
+            document.getElementById("condition").innerText = "Condition: " + response.current.condition.text;
 
 
 
-        document.getElementById("fDayTemp").innerText = "Temp: " + response.forecast.forecastday[0].day.maxtemp_f;
-        document.getElementById("fDayCond").innerText = "Condition: " + response.forecast.forecastday[0].day.condition.text;
-        document.getElementById("fDayIco").src = response.forecast.forecastday[0].day.condition.icon;
-        changeColor((response.forecast.forecastday[0].day.maxtemp_f), "fDay");
-        document.getElementById("sDayTemp").innerText = "Temp: " + response.forecast.forecastday[1].day.maxtemp_f;
-        document.getElementById("sDayCond").innerText = "Condition: " + response.forecast.forecastday[1].day.condition.text;
-        document.getElementById("sDayIco").src = response.forecast.forecastday[1].day.condition.icon;
-        changeColor((response.forecast.forecastday[1].day.maxtemp_f), "sDay");
-        document.getElementById("thrDayTemp").innerText = "Temp: " + response.forecast.forecastday[2].day.maxtemp_f;
-        document.getElementById("thrDayCond").innerText = "Condition: " + response.forecast.forecastday[2].day.condition.text;
-        document.getElementById("thrDayIco").src = response.forecast.forecastday[2].day.condition.icon;
-        changeColor((response.forecast.forecastday[2].day.maxtemp_f), "thrDay");
+            document.getElementById("fDayTemp").innerText = "Temp: " + response.forecast.forecastday[0].day.maxtemp_f;
+            document.getElementById("fDayCond").innerText = "Condition: " + response.forecast.forecastday[0].day.condition.text;
+            document.getElementById("fDayIco").src = response.forecast.forecastday[0].day.condition.icon;
+            changeColor((response.forecast.forecastday[0].day.maxtemp_f), "fDay");
+            document.getElementById("sDayTemp").innerText = "Temp: " + response.forecast.forecastday[1].day.maxtemp_f;
+            document.getElementById("sDayCond").innerText = "Condition: " + response.forecast.forecastday[1].day.condition.text;
+            document.getElementById("sDayIco").src = response.forecast.forecastday[1].day.condition.icon;
+            changeColor((response.forecast.forecastday[1].day.maxtemp_f), "sDay");
+            document.getElementById("thrDayTemp").innerText = "Temp: " + response.forecast.forecastday[2].day.maxtemp_f;
+            document.getElementById("thrDayCond").innerText = "Condition: " + response.forecast.forecastday[2].day.condition.text;
+            document.getElementById("thrDayIco").src = response.forecast.forecastday[2].day.condition.icon;
+            changeColor((response.forecast.forecastday[2].day.maxtemp_f), "thrDay");
+        }
+        else {
+            document.getElementById("overlay").style.display = "inline-block";
+            document.getElementById("errorMessage").innerText = `Feed back from API not recognized`;
+        }
       }
 
 
